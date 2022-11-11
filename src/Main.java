00e7c612b25fbe0058b94fcc4184292a0061a7c3
Sergey <sergey.chernykh.89@icloud.com>
2022-11-11 22:53:48 +0500

    Add min & max values

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,42 @@ public class Main
 
         System.out.println(sum);
 
+        byte byteMinValue = Byte.MIN_VALUE;
+        byte byteMaxValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte min value is: " + byteMinValue);
+        System.out.println("Byte max value is: " + byteMaxValue);
+
+        short shortMinValue = Short.MIN_VALUE;
+        short shortMaxValue = Short.MAX_VALUE;
+
+        System.out.println("Short min value is: " + shortMinValue);
+        System.out.println("Short max value is: " + shortMaxValue);
+
+        int intMinValue = Integer.MIN_VALUE;
+        int intMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer min value is: " + intMinValue);
+        System.out.println("Integer max value is: " + intMaxValue);
+
+        long longMinValue = Long.MIN_VALUE;
+        long longMaxValue = Long.MAX_VALUE;
+
+        System.out.println("long min value is: " + longMinValue);
+        System.out.println("long max value is: " + longMaxValue);
+
+        float floatMinValue = Float.MIN_VALUE;
+        float floatMaxValue = Float.MAX_VALUE;
+
+        System.out.println("Float min value is: " + floatMinValue);
+        System.out.println("Float max value is: " + floatMaxValue);
+
+        double doubleMinValue = Double.MIN_VALUE;
+        double doubleMaxValue = Double.MAX_VALUE;
+
+        System.out.println("Double min value is: " + doubleMinValue);
+        System.out.println("Double max value is: " + doubleMaxValue);
+
     }
 
     public static Integer sumDigits(Integer number)
